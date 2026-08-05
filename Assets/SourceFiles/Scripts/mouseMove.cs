ae37be79044eb844f0601ec2be11db57f682a4db
atty-patty <ag.atharva11@gmail.com>
2026-08-05 08:47:37 +0530

    added player controller

---
+++ Assets/SourceFiles/Scripts/mouseMove.cs
@@ -0,0 +1,30 @@
+using System;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class mouseMove : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    float MouseX,MouseY;
+    public float MouseSensitivity = 300;
+    public Transform player;
+    float Xrotation;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
+        MouseY = Input.GetAxis("Mouse Y")* MouseSensitivity * Time.deltaTime;
+
+        Xrotation -= MouseY;
+        Xrotation = Math.Clamp(Xrotation,-90f,90f);
+
+        transform.localRotation = Quaternion.Euler(Xrotation,0,0);
+
+        player.Rotate(Vector3.up*MouseX);
+    }
+}
---
+++ Assets/SourceFiles/Scripts/playerMovement.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+    public float gravity = -10f;
+    public float ground_distance = 0.5f;
+
+    Vector3 velocity;
+    float x,z;
+    bool isGrounded;
+
+    public Transform GroundCheck;
+    public LayerMask groundmask;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(GroundCheck.position,ground_distance,groundmask);
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.forward * z + transform.right * x;
+
+        controller.Move(move*speed*Time.deltaTime);
+
+        velocity.y += gravity*Time.deltaTime;
+        controller.Move(velocity*Time.deltaTime);
+    }
+}
